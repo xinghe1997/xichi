c9dc4f383d2f88a83f1625d44925acab8fc62aa8
xinghe1997 <57445290+xinghe1997@users.noreply.github.com>
2020-05-08 12:48:17 +0800

    Add files via upload

--- xichi/xichi/spiders/daili.py
+++ xichi/xichi/spiders/daili.py
@@ -1,19 +1,39 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
-
+import requests
 class DailiSpider(scrapy.Spider):
     name = 'daili'
     allowed_domains = ['https://www.xicidaili.com']
-    start_urls = ['https://www.xicidaili.com/nt']
+    start_urls = ['https://www.xicidaili.com/wn/']
 
     def parse(self, response):
         item = []
-        trs = response.xpath("//tr[@class='odd']")
+       #保存路径
+        f = open('ip.json','wb')
+        #测试proxy链接
+        test_url ="https://www.baidu.com/"
+        #提取所有tr
+        trs = response.xpath("//tr[position() > 1]")
+        #遍历
         for tr in trs:
-            item.append({'ip':tr.xpath("td[2]/text()").get(),'post':tr.xpath("td[3]/text()").get()})
-        self.file = open('ip.json','wb')
-        line = json.dumps(item)+"\n";
-        self.file.write(line.encode('utf-8'))
+            proxy = {'http':tr.xpath("td[2]/text()").get()+':'+tr.xpath("td[3]/text()").get()};
+            #捕获
+            try:
+                test = requests.get(test_url, proxies=proxy, timeout=5)
+                #判断状态码，等于200就保存
+                if test.status_code == 200:  
+                    item.append(proxy)
+            except:
+                #测试proxy捕获到错误时跳过本次循环
+                continue         
+        line = json.dumps(item)
+        #保存文件，编码utf-8
+        f.write(line.encode('utf-8'))
+       
         
+        
+           
+               
+            
       
